56e4a95ed316ad61dfab81b7053262da2fec1e2c
Stanislav Okulov <stasokulov@gmail.com>
2020-07-26 10:46:22 +0300

    Сделал слайдер, доработал плики с профессиями и список преподавателей.

---
+++ script/slider.js
@@ -0,0 +1,56 @@
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('.slide');
+const slidesArray = Array.from(slides);
+let counter = 0;
+
+const banner = document.querySelector('.banner');
+const eventDay = banner.querySelector('.banner__event-day');
+const eventMonth = banner.querySelector('.banner__event-month');
+const eventTitle = banner.querySelector('.banner__event-title');
+const eventSubtitle = banner.querySelector('.banner__event-subtitle');
+const sliderDataBase = [
+    {
+        day: 25,
+        month: 'января',
+        title: 'день студента',
+        subtitle: 'Подздравляем всех учащихся с Днем студента. Желаем творческих и профессиональных успехов.'
+    }, {
+        day: 1,
+        month: 'апреля',
+        title: 'день дурака',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ipsum iusto labore nulla!'
+    }, {
+        day: 31,
+        month: 'декабря',
+        title: 'Новый год',
+        subtitle: 'Изначально Раиса Кудашева опубликовала стихотворение «Ёлка» в детском журнале «Малютка» в 1903 году.'
+    }
+]
+
+function showFirstSlide() {
+    slidesArray.forEach(slide => slide.classList.remove('active'));
+    counter = 0;
+    slidesArray[counter].classList.add('active');
+    fillBanner(counter);
+}
+function turnSlides() {
+    if(counter < slidesArray.length - 1) {
+        slidesArray[counter].classList.remove('active');
+        slidesArray[counter + 1].classList.add('active');
+        counter++;
+        fillBanner(counter);
+    } else {
+        counter = 0;
+        showFirstSlide();
+    }
+}
+function fillBanner(counter) {
+    let currentSlide = sliderDataBase[counter];
+    eventDay.innerHTML = currentSlide.day;
+    eventMonth.innerHTML = currentSlide.month;
+    eventTitle.innerHTML = currentSlide.title;
+    eventSubtitle.innerHTML = currentSlide.subtitle;
+}
+
+showFirstSlide();
+setInterval(turnSlides,4000);
