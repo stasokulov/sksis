b42f49a8c7c01fac7ed8c8fa979944de71cda333
Stanislav Okulov <stasokulov@gmail.com>
2020-07-27 09:22:17 +0300

    Рефакторинг скриптов. Добавил комментарии к скриптам.

--- script/sticky-header.js
+++ script/sticky-header.js
@@ -1,23 +1,26 @@
+// Когда целевой блок хедера доскролливается до верха страницы, вынимаем его из потока и фиксируем его относительно экрана.
+// Чтобы нижележащие блоки не прыгнули вверх, заменяем вынутый блок пустым блоком той же высоты.
 window.addEventListener('scroll', get_sticky_header);
 
 function get_sticky_header () {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    let header = document.querySelector("header");
-    let fixed = document.querySelector("header.fixed");
-    let emptyHeader = document.querySelector(".empty-header");
-    let sticky = document.querySelector(".sticky");
-    let headerPaddingTop = 22;
-    let mobile = window.innerWidth < 992;
+    const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    const header = document.querySelector("header");
+    const fixed = document.querySelector("header.fixed");
+    const emptyHeader = document.querySelector(".empty-header");
+    const sticky = document.querySelector(".sticky");
+    const headerPaddingTop = 22;
+    const mobile = window.innerWidth < 992;
     if (header) {
         if (scrolled > sticky.offsetTop - headerPaddingTop &&
             fixed === null &&
             mobile) {
-            let emptyBlock = document.createElement('div');
+            const emptyBlock = document.createElement('div');
             emptyBlock.classList.add('empty-header');
             emptyBlock.style.height = header.offsetHeight + 'px';
             header.insertAdjacentElement('beforebegin', emptyBlock);
             header.classList.add('fixed');
-        } else if (scrolled <= sticky.offsetTop + headerPaddingTop && fixed !== null) {
+        } else if (scrolled <= sticky.offsetTop + headerPaddingTop &&
+            fixed !== null) {
             header.classList.remove('fixed');
             // emptyHeader.remove();
             emptyHeader.parentElement.removeChild(emptyHeader); //For IE
