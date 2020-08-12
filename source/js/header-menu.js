9e176b4a28cba5b85da68723bcefbb269c53d5bc
Stanislav Okulov <stasokulov@gmail.com>
2020-08-12 09:35:30 +0300

    Убрал ненужные для прода файлы

--- source/js/header-menu.js
+++
@@ -1,8 +0,0 @@
-// При клике на бургер скрываем блоки ниже хедера.
-const burger = document.querySelector('.burger');
-const main = document.querySelector('main');
-const footer = document.querySelector('footer');
-burger.addEventListener('click', function() {
-    main.classList.toggle('hidden');
-    footer.classList.toggle('hidden');
-});
